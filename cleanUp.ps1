a8ee02fecf429f3284289805aa598d1650ef33b5
Nelson Orellana <55712980+Nels2@users.noreply.github.com>
2022-11-08 10:45:28 -0700

    Update cleanUp.ps1
    add output messages to help debug.

--- cleanUp.ps1
+++ cleanUp.ps1
@@ -10,13 +10,20 @@
 
 #write2log func
 Start-Transcript -Append C:\WCUA\$env:computername.log
+Write-Host "Writing output to $env:computername.log .."
 # Display current free space.
+Write-Host "Here is Disk space before cleanUp.ps1 does anything.. :"
 Get-CimInstance -ClassName Win32_LogicalDisk | where caption -eq "C:" | foreach-object {write " $($_.caption) $('{0:N2}' -f ($_.Size/1gb)) GB Total, $('{0:N2}' -f ($_.FreeSpace/1gb)) GB free "}
 
 #Auto delete any and all temp files.
+Write-Host "Running Disk Cleanup to auto delete temp files.."
 cleanmgr /sagerun
 
 # stop, and disable SysMain, or otherwise known as SuperFetch
+Write-Host "Current SysMain Status before:"
+Get-Service -name SysMain | Select Name, Status, StartType
+Write-Host "==============================================="
+Write-Host "Disabled SysMain StartupType, status now:"
 Set-Service -name SysMain -StartupType disabled
 Stop-Service -name SysMain -force
 Get-Service -name SysMain | Select Name, Status, StartType
