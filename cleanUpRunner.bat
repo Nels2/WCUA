82d310af917b46ba8770c91bc4e5c9bbd78d9f0f
Nelson Orellana <55712980+Nels2@users.noreply.github.com>
2022-11-08 15:24:22 -0700

    Update cleanUpRunner.bat
    update for crrect

--- cleanUpRunner.bat
+++ cleanUpRunner.bat
@@ -1,4 +1,4 @@
-chkdsk /f
+sfc /scannow
 powershell -command "& {Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser -Force}"
 powershell -command "& {Unblock-File -Path C:\WCUA\cleanUp.ps1}"
 powershell -File C:\WCUA\cleanUp.ps1
