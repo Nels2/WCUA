219cf0ef5dfdcdad77118f8e257ff04ef86d2565
Nelson Orellana <55712980+Nels2@users.noreply.github.com>
2022-11-08 10:31:49 -0700

    Update cleanUpRunner.bat
    rewrite

--- cleanUpRunner.bat
+++ cleanUpRunner.bat
@@ -1,15 +1,27 @@
 powershell -command "& {Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser -Force}"
-powershell -command "& {Unblock-File -Path cleanUp.ps1}"
-powershell -File cleanUp.ps1
+powershell -command "& {Unblock-File -Path C:\WCUA\cleanUp.ps1}"
+powershell -File C:\WCUA\cleanUp.ps1
 @echo off
->proc_$env:computername.log (
-del C:\Windows\prefetch\*.*/s /q 
-net stop wsearch
+call :tempDel >> C:\WCUA\%ComputerName%.log
+call :wSearchRebuild >> C:\WCUA\%ComputerName%.log
+call :wsearch >> C:\WCUA\%ComputerName%.log
+
+GOTO :END
+
+:tempDel
+del C:\Windows\prefetch\*.*/s /q
+%localappdata%\Microsoft\OneDrive\onedrive.exe /reset 
+EXIT /B
+
+:wSearchRebuild
+net stop wsearch 
 del "%ProgramData%\Microsoft\Search\Data\Applications\Windows\Windows.edb"
+EXIT /B
+
 :wsearch
 net start wsearch
-IF NOT %ERRORLEVEL%==0 (goto :wsearch) ELSE goto :END
+EXIT /B
+
 :END
-powershell -command "& {Unblock-File -Path finalHard.ps1}"
-powershell -File finalHard.ps1
-)
+powershell -command "& {Unblock-File -Path C:\WCUA\finalHard.ps1}" 
+powershell -File C:\WCUA\finalHard.ps1 
