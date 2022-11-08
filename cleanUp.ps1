75fcad3ef71baae1944fe9ae46a7d8a75954ea03
Nelson Orellana <55712980+Nels2@users.noreply.github.com>
2022-11-08 10:32:16 -0700

    Update cleanUp.ps1
    change where log is saved

--- cleanUp.ps1
+++ cleanUp.ps1
@@ -9,7 +9,7 @@
 # Important You may need to restart your device after running.
 
 #write2log func
-Start-Transcript -Append proc_$env:computername.log
+Start-Transcript -Append C:\WCUA\$env:computername.log
 # Display current free space.
 Get-CimInstance -ClassName Win32_LogicalDisk | where caption -eq "C:" | foreach-object {write " $($_.caption) $('{0:N2}' -f ($_.Size/1gb)) GB Total, $('{0:N2}' -f ($_.FreeSpace/1gb)) GB free "}
 
@@ -17,10 +17,9 @@ Get-CimInstance -ClassName Win32_LogicalDisk | where caption -eq "C:" | foreach-
 cleanmgr /sagerun
 
 # stop, and disable SysMain, or otherwise known as SuperFetch
-sc config "SysMain" start= disabled
-sc stop "SysMain"
-
-# reset online sync with oneDrive
-%localappdata%\Microsoft\OneDrive\onedrive.exe /reset 
-# after this script is done running, via the bat file the windows search index will be rebuilt and delete all prefetch files.
+Set-Service -name SysMain -StartupType disabled
+Stop-Service -name SysMain -force
+Get-Service -name SysMain | Select Name, Status, StartType
+# after this script is done running, via the bat file the one drive sync will be reset, windows search index will be rebuilt and delete all prefetch files.
 Stop-Transcript
+Start-Sleep -Seconds 10
