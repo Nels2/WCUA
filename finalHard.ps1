7417c96f371a8469d0d4be459d70e055dbb24acf
Nelson Orellana <55712980+Nels2@users.noreply.github.com>
2022-11-08 10:32:50 -0700

    Update finalHard.ps1

--- finalHard.ps1
+++ finalHard.ps1
@@ -1,4 +1,4 @@
 # Display current free space.
-Start-Transcript -Append proc_$env:computername.log
+Start-Transcript -Append C:\WCUA\$env:computername.log
 Get-CimInstance -ClassName Win32_LogicalDisk | where caption -eq "C:" | foreach-object {write " $($_.caption) $('{0:N2}' -f ($_.Size/1gb)) GB Total, $('{0:N2}' -f ($_.FreeSpace/1gb)) GB free "}
 Stop-Transcript
